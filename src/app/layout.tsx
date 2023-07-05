93d1127c43403a2b93d0ead084af8ad74cb775bf
zsio <zzi@live.com>
2023-07-05 19:08:42 +0800

    添加 analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import {Inter} from 'next/font/google'
 
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +11,16 @@ export const metadata = {
   description: '收集免费、无需登录的GPT镜像站。此站点永久免费且保持更新。',
 }
 
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="zh-cn">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
